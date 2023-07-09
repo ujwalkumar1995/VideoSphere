e2a8fcdd7acbe92f3fea926491a9345187931c90
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-07-09 17:24:01 +0530

    Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App" id="root">
+    <div className='App' id='root'>
       VideoSphere
     </div>
   );
