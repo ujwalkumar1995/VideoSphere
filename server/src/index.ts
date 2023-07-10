96e88659a8d1d7d75ce5523433a51b93f42ed2e5
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-07-10 23:04:39 +0530

    Create basic server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,25 @@
 import express, { Express, Request, Response } from 'express';
+import * as http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app: Express = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World!' });
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
-app.listen(8080, async () => {
-  console.log('Server is running at http://localhost:8080');
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Server is running');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
