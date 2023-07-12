ccb4368a703ac23288bd781f94185e05e85da545
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-07-13 01:29:45 +0530

    Add answercall in SocketContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,39 @@ import Options from './components/Options';
 import VideoPlayer from './components/VideoPlayer';
 import './styles/App.css';
 import { Typography, AppBar } from '@mui/material';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: { breakpoints: { down: (arg0: string) => any } }) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div className='App' id='root'>
-      <AppBar position='static' color='inherit'>
+    <div className={classes.wrapper} id='root'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography variant='h2' align='center'>
           VideoSphere
         </Typography>
