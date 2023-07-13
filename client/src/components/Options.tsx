38ea1cbecfc7f6564869e6d84424d60149032cc7
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-07-14 00:52:04 +0530

    Add leave call and join call buttons in Options

--- client/src/components/Options.tsx
+++ client/src/components/Options.tsx
@@ -1,15 +1,120 @@
-import { ReactNode } from 'react';
+import { ReactNode, useContext, useState } from 'react';
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Assignment, Phone, PhoneDisabled } from '@mui/icons-material';
+import { SocketContext } from '../contexts/SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  gridContainer: {
+    width: '100%',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  container: {
+    width: '600px',
+    margin: '35px 0',
+    padding: 0,
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+    },
+  },
+  margin: {
+    marginTop: 20,
+  },
+  padding: {
+    padding: 20,
+  },
+  paper: {
+    padding: '10px',
+    border: '2px solid black',
+  },
+}));
 
 interface Props {
   children: ReactNode;
 }
 
 const Options: React.FC<Props> = ({ children }: Props) => {
+  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+
+  const [idToCall, setIdToCall] = useState('');
+  const classes = useStyles();
+
   return (
-    <div>
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete='off'>
+          <Grid container className={classes.container}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant='h6'>
+                Account Info
+              </Typography>
+              <TextField
+                label='Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              <div className={classes.margin}>
+                <CopyToClipboard text={me}>
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    fullWidth
+                    startIcon={<Assignment fontSize='large' />}
+                  >
+                    Copy your Id
+                  </Button>
+                </CopyToClipboard>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant='h6'>
+                Make a call
+              </Typography>
+              <TextField
+                label='Id to Call'
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant='contained'
+                  color='secondary'
+                  startIcon={<PhoneDisabled fontSize='large' />}
+                  fullWidth
+                  onClick={leaveCall}
+                  className={classes.margin}
+                >
+                  Hang up
+                </Button>
+              ) : (
+                <Button
+                  variant='contained'
+                  color='primary'
+                  startIcon={<Phone fontSize='large' />}
+                  fullWidth
+                  onClick={() => callUser(idToCall)}
+                  className={classes.margin}
+                >
+                  Call
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
       Options
       {children}
-    </div>
+    </Container>
   );
 };
 
